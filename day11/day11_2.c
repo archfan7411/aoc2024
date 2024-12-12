3f5efa0360bea4e8e7af02a31943567201e91865
archfan7411 <33993466+archfan7411@users.noreply.github.com>
2024-12-11 19:53:11 -0500

    Short-circuit '1' stones (didn't speed up as much as expected)

--- day11/day11_2.c
+++ day11/day11_2.c
@@ -3,6 +3,15 @@
 
 long input[] = {7725, 185, 2, 132869, 0, 1840437, 62, 26310};
 
+// Short-circuit "1" stones
+const int NUM_SHORTCIRCUIT = 10;
+
+// Stones after starting with 1 and blinking NUM_SHORTCIRCUIT times
+long stones_after[] = {56, 94, 57, 24, 84, 91, 86, 36, 4, 2, 0, 2, 8, 26, 77, 32, 8, 26, 77, 32, 12144, 18216, 1, 16192, 4, 2, 0, 2, 12144, 18216, 1, 16192, 6, 9, 0, 8, 8, 4, 0, 4, 24, 20, 8, 4, 0, 4, 8, 26, 77, 32, 12144, 18216, 1, 16192, 4, 2, 0, 2, 12144, 18216, 1, 16192};
+
+// Number of stones after i+1 blinks
+long num_stones_after[] = {1, 2, 4, 4, 7, 14, 16, 20, 39, 62};
+
 long count_stones(long stone, int blinks) {
     if (blinks == 0) {
         return 1;
@@ -10,6 +19,19 @@ long count_stones(long stone, int blinks) {
 
     if (stone == 0) {
         return count_stones(1, blinks-1);
+    }
+
+    if (stone == 1) {
+        if (blinks < NUM_SHORTCIRCUIT) {
+            return num_stones_after[blinks-1];
+        }
+        // otherwise:
+        long num = 0;
+        for (int i=0; i < sizeof(stones_after)/sizeof(long); i++) {
+            long res = count_stones(stones_after[i], blinks-NUM_SHORTCIRCUIT);
+            num += res;
+        }
+        return num;
     }
 
     int num_digits = floor(log10(stone)+1);
@@ -28,12 +50,10 @@ long count_stones(long stone, int blinks) {
 
 int main(void) {
     long num_stones = 0;
-
     for (int i=0; i < sizeof(input)/sizeof(long); i++) {
-        num_stones += count_stones(input[i], 75);
+        num_stones += count_stones(input[i], 30);
     }
 
     printf("\n%ld", num_stones);
-
     return 0;
 }
